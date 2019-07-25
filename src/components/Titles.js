9a65fc0a03dbfa22ee88615b83cb52efff90d10c
Pamyk <pamyk.ch@gmail.com>
2019-07-25 11:39:57 -0400

    just weather app

---
+++ src/components/Titles.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Weather Finder</h1>
+		<h3 className="title-container__subtitle">Would you like to check weather?</h3>
+	</div>
+);
+
+export default Titles;
